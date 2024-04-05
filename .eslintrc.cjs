43ee9451df0442a260476b6c87f59904b17b6c4d
PoliJun <rivenloving@gmail.com>
2024-04-05 17:49:31 +0800

    Fix linting issues and add import rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,16 +7,18 @@ module.exports = {
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "prettier",
+
     ],
     ignorePatterns: ["dist", ".eslintrc.cjs"],
     parserOptions: { ecmaVersion: "latest", sourceType: "module" },
     settings: { react: { version: "18.2" } },
-    plugins: ["react-refresh"],
+    plugins: ["react-refresh","import"],
     rules: {
         "react/jsx-no-target-blank": "off",
         "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true },
         ],
+       "import/newline-after-import": "warn",
     },
 };
