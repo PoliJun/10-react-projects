a46c45526849e01e396378dad94dd80e2b845fda
PoliJun <rivenloving@gmail.com>
2024-04-06 12:59:12 +0800

    Update ESLint configuration and React import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
