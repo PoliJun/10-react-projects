ae90ef6071a257ab4904f3fac33be0d6ff466352
PoliJun <rivenloving@gmail.com>
2024-04-07 16:27:40 +0800

    Add ToggleColorPage component and related files. ToggleColor

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./styles/App.scss";
-import CounterPage from "./pages/CounterPage";
-import TodoPage from "./pages/TodoPage";
-import MealsPage from "./pages/MealsPage";
 import CalculatorPage from "./pages/CalculatorPage";
+import CounterPage from "./pages/CounterPage";
+import MealsPage from "./pages/MealsPage";
+import TodoPage from "./pages/TodoPage";
+import ToggleColorPage from "./pages/ToggleColorPage";
+import "./styles/App.scss";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<CounterPage />} />
           <Route path="/counter" element={<CounterPage />} />
           <Route path="/todo" element={<TodoPage />} />
           <Route path="/meals" element={<MealsPage />} />
           <Route path="/calculator" element={<CalculatorPage />} />
+          <Route path="/color" element={<ToggleColorPage />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/ToggleColorPage.jsx
@@ -0,0 +1,8 @@
+import ToggleColor from '../components/ToggleColor'
+
+const ToggleColorPage = () => {
+  return (
+    <><ToggleColor /></>
+  )
+}
+export default ToggleColorPage
